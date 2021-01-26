2a4ea54e8f1aebbd26194b66ac6a2272bf5e231c
gerkim62 <72649107+gerkim62@users.noreply.github.com>
2021-01-26 19:24:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+const faq = document.querySelectorAll('.onefaq');
+let faqLength = faq.length;
+for (let i = 0; i < faqLength; i++) {
+  faq[i].addEventListener('click', () => {
+    let currentP = document.querySelector('.' + faq[i].classList[1] + ' p');
+    let currentImg = document.querySelector('.in' + i);
+    let currentH2 = document.querySelector('.' + faq[i].classList[1] + ' h2');
+    
+    if(currentP.style.display == 'block') {
+      currentP.style.display = 'none';
+      currentImg.style.transform = 'rotate(0deg)'
+      currentH2.style.fontWeight = '400';
+    }else{
+      currentP.style.display = 'block'
+      currentImg.style.transform = 'rotate(180deg)';
+      currentH2.style.fontWeight = '700';
+    }
+  })
+}
